f47cdbe5d5ea330ec74c6ff1afcd0468f9baaef1
Eduardo Casanova <ecasanovac@gmail.com>
2016-08-02 14:14:02 +0200

    Update

--- routes/api.js
+++ routes/api.js
@@ -5,36 +5,14 @@ var md5 = require('js-md5');
 
 var users = {};
 
-/* GET users listing. */
-router.get('/register', function(req, res, next) {
-    var userID = req.query.userID;
-    var deviceID = req.query.deviceID;
-    var now = (new Date()).getTime();
-    var token = md5(userID + deviceID + now);
-
-    var user = users[userID] || {userID: userID};
-
-    console.log(users);
-
-    user.deviceID = deviceID;
-    user.loginTimestamp = now;
-    user.token = token;
-    users[userID] = user;
-
-    res.send({
-        result: "success",
-        token: token
-    });
-});
-
 router.post('/register', function(req, res, next) {
-    var userID = req.query.userID;
-    var deviceID = req.query.deviceID;
+    var userID = req.body.userID;
+    var deviceID = req.body.deviceID;
     var now = (new Date()).getTime();
-    var token = md5(userID + deviceID + now);
+    var token = md5("" + now + userID + deviceID);
 
     var user = users[userID] || {userID: userID};
-
+    console.log("users");
     console.log(users);
 
     user.deviceID = deviceID;
